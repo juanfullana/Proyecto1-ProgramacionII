e52faefe516c46bb90ab6d14a39b5af5bf197ec6
Juan Fullana <juanfullana6@gmail.com>
2021-10-18 12:22:44 -0300

    Ejercicio 1
    Matriculas Aviones

---
+++ Programacion2/Proyecto1_Programacion2.py
@@ -0,0 +1,27 @@
+""" 
+Expresiones Regulares.
+
+Ejercicio 1:
+
+Implemente una expresión regular para validar matrículas argentinas.
+
+Ej matrículas válidas:
+
+LV-QWE
+LQ-ABE
+LV-X443
+LV-S586
+LV-SX334
+
+Ejemplos matrículas inválidas:
+
+LA-123
+LX-ABC
+LV
+LV-344
+LV-SX33UIO4
+LV-SX334UIO
+
+SOLUCION = (L(V|Q)-[A-Z]{3})|(LV-(X|S|SX)[\d]{3}$) 
+
+"""
