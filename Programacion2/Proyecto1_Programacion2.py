e9a100b2e9cfc2d9751b12e1934dc74c0da50a8b
Juan Fullana <juanfullana6@gmail.com>
2021-10-19 09:19:38 -0300

    Proyecto 1 Programacion 2
    Agregada solucion ejercicio 2 (ER)

--- Programacion2/Proyecto1_Programacion2.py
+++ Programacion2/Proyecto1_Programacion2.py
@@ -22,8 +22,29 @@ LV-344
 LV-SX33UIO4
 LV-SX334UIO
 
-SOLUCION = (L(V|Q)-[A-Z]{3})|(LV-(X|S|SX)[\d]{3}$)
+SOLUCION = (L(V|Q)-[A-Z]{3})|(LV-(X|S|SX)[\d]{3}$) [Si encontras una mejor, cambiala]
 
-Probando si anda
+
+2. Diseñe una ER para validar cadenas de números naturales menores a 1900
+
+
+Numeros validos:
+
+120
+589
+899
+925
+1503
+1900
+1899
+
+Numeros Invalidos:
+
+3555
+8888
+256.24
+0
+
+SOLUCION = ^[1][0-8]?[0-9]?[0-9]?$|^[1-9][0-9]?[0-9]?$|1900 [Si encontras una mejor, cambiala]
 
 """
