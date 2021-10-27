70676ebe027bc3901e4e9d324bbee6f9a0228655
Andoni Roque <roque.andoni@gmail.com>
2021-10-27 13:45:15 -0300

    funcion pi, resuelto

--- Programacion2/Proyecto1_Programacion2.py
+++ Programacion2/Proyecto1_Programacion2.py
@@ -88,37 +88,21 @@ hasta retornar un unico resultado.
 Ejercicio 2 - Calcular el numero irracional "pi" sin usar estructuras repetitivas.
 '''
 
-N = int(input("Ingrese la cantidad de terminos que desea caluclar: "))
-lista_N = list()
-
-
-def generar_terminos(numeros):  # Cantidad de veces a calcular
-    i = 1
-    while i <= numeros:
-        lista_N.append(i)
-        i = i + 1
-    return lista_N
-
 
 def pi_funcion(i):  # funcion de la ecuacion
     numerador = 4 * ((-1)**i)
     denominador = (2*i) + 1
-    funcion = numerador / denominador
-    return funcion
+    return numerador / denominador
 
 
-lista_N = generar_terminos(N)
-print("Esta es la lista_N", lista_N)
+terminos = int(input("Ingrese la cantidad de terminos que desea caluclar: "))
 
-# La lista_N contiene los valores de i
-# aplico la funcion de la ecuacion a cada valor de i
-lista_resultados = list(map(pi_funcion, lista_N))
-print("Resultado de funciones:", lista_resultados)
+lista_terminos = range(0, terminos)
 
-# Sumo todos los valores para, supuestamente, obtener pi.
+lista_resultados = list(map(pi_funcion, lista_terminos))
+
+
 from functools import reduce
+
 resultado_final = reduce(lambda a, b: a+b, lista_resultados)
 print("El resultado final es:", resultado_final)
-
-# Esta re verde esto pero lo charlamos si hay algo que no se entendió
-# o si la logica que aplique no es la correcta
