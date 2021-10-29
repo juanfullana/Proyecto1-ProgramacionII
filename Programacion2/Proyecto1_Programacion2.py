44721f500765679bb4e79391fe23e9a60fe9dc6d
Juan Fullana <juanfullana6@gmail.com>
2021-10-29 16:27:32 -0300

    Principios Menu
    Agregando un poco de menu

--- Programacion2/Proyecto1_Programacion2.py
+++ Programacion2/Proyecto1_Programacion2.py
@@ -1,90 +1,98 @@
-""" 
-Expresiones Regulares.
+import time
+print("Bienvenidos a nuestro primer proyecto!")
+time.sleep(2)
+print("Esperamos que sea de su agrado.")
+time.sleep(2)
+print("A continuación podrá disfrutar de nuestro menú interactivo.")
+time.sleep(2)
+print("Contamos con 4 ejercicios y sus respectivos incisos.")
+time.sleep(2)
+# opcion_ejercicio= 0
+# while opcion_ejercicio != 5: 
+print("[1] El primer ejercicio cuenta con 2 incisos y está destinado al uso de expresiones regulares.")
+print("[2] El segundo ejercicio cuenta con 4 incisos y está destinado al uso de funciones recursivas")
+print("[3] El tercer ejercicio cuenta con 2 incisos y está destinado al uso de colecciones")
+print("[4] El cuarto y ultimo ejercicio cuenta con 2 incisos y está destinado al intercambio de datos")
+opcion_ejercicio = input("Ingrese el número de ejercicio al cual quiere acceder: ")  
+ejercicio1 = """
 
-Ejercicio 1:
+    Expresiones Regulares.
 
-Implemente una expresión regular para validar matrículas argentinas.
+    Ejercicio 1:
 
-Ej matrículas válidas:
+    Implemente una expresión regular para validar matrículas argentinas.
 
-LV-QWE
-LQ-ABE
-LV-X443
-LV-S586
-LV-SX334
+    Ej matrículas válidas:
 
-Ejemplos matrículas inválidas:
+    LV-QWE
+    LQ-ABE
+    LV-X443
+    LV-S586
+    LV-SX334
 
-LA-123
-LX-ABC
-LV
-LV-344
-LV-SX33UIO4
-LV-SX334UIO
+    Ejemplos matrículas inválidas:
 
-SOLUCION = (L(V|Q)-[A-Z]{3})|(LV-(X|S|SX)[\d]{3}$)z [Si encontras una mejor, cambiala]
+    LA-123
+    LX-ABC
+    LV
+    LV-344
+    LV-SX33UIO4
+    LV-SX334UIO
 
-2. Diseñe una ER para validar cadenas de números naturales menores a 1900
+    SOLUCION = (L(V|Q)-[A-Z]{3})|(LV-(X|S|SX)[\d]{3}$)z 
 
-Numeros validos:
+    2. Diseñe una ER para validar cadenas de números naturales menores a 1900
 
-120
-589
-899
-925
-1503
-1900
-1899
+    Numeros validos:
 
-Numeros Invalidos:
+    120
+    589
+    899
+    925
+    1503
+    1900
+    1899
 
-3555
-8888
-256.24
-0
+    Numeros Invalidos:
 
-SOLUCION = ^[1][0-8]?[0-9]?[0-9]?$|^[1-9][0-9]?[0-9]?$|1900 [Si encontras una mejor, cambiala]
-"""
+    3555
+    8888
+    256.24
+    0
+
+    SOLUCION = ^[1][0-8]?[0-9]?[0-9]?$|^[1-9][0-9]?[0-9]?$|1900
+
+    """
+if opcion_ejercicio == "1": 
+    print(ejercicio1)
+
+elif opcion_ejercicio == "2":
+
+    enunciado_recursividad_1='''
+        RECUSIVIDAD 
+
+        Ejercicio 1 - Codificar un numero entero de la siguiente manera:
+        - Cada digito par sustituirlo por 1
+        - Cada digito impar sustituirlo por 2
+        Puede pasar el numero a otras representaciones para resolver el ejercicio
+        '''
+    print(enunciado_recursividad_1)
+    time.sleep(4)
+    num = input("Ingrese el numero a codificar: ")
+    num = str(num)
 
 
-
-
-
-
-
-
-
-
-
-'''
-RECUSIVIDAD 
-
-Ejercicio 1 - Codificar un numero entero de la siguiente manera:
-- Cada digito par sustituirlo por 1
-- Cada digito impar sustituirlo por 2
-Puede pasar el numero a otras representaciones para resolver el ejercicio
-'''
-num = 436225
-num = str(num)
-
-
-def codificar(Z):
-    if len(Z) == 0:
-        return ""
-    else:
-        if int(Z[0]) % 2 == 0:
-            return "1" + codificar(Z[1:])
+    def codificar(Z):
+        if len(Z) == 0:
+            return ""
         else:
-            return "2" + codificar(Z[1:])
+            if int(Z[0]) % 2 == 0:
+                return "1" + codificar(Z[1:])
+            else:
+                return "2" + codificar(Z[1:])
 
 
-print(codificar(num))
-
-
-
-
-
-
+    print(codificar(num))
 
 '''
 Ejercicio 3 - Decidir si dos listas de numeros enteros son iguales
@@ -107,11 +115,6 @@ lista_2 = [2, 3, 4, 6]
 
 print(verifiacar_listas(lista_1, lista_2))
 
-
-
-
-
-
 '''
 Ejercicio 4 - Realizar la division entera entre dos numeros enteros positivos A y B, (B != 0).
 
@@ -129,15 +132,8 @@ def division_entera(A, B):
 
 print(division_entera(A, B))
 
-
-
-
-
-
-
-
-
 '''
+
 # Colecciones:
 # Explicar en pocas palabras y utilizando diagarmas las operaciones de map, filter y reducce.
 # Proponga ejemplos de cada uno (conceptuales no necesariamente en código)
@@ -165,10 +161,6 @@ hasta retornar un unico resultado.
 Ver diagrama 3.
 '''
 
-
-
-
-
 '''
 Ejercicio 2 - Calcular el numero irracional "pi" sin usar estructuras repetitivas.
 '''
@@ -186,21 +178,29 @@ lista_terminos = range(0, terminos)
 
 lista_resultados = list(map(pi_funcion, lista_terminos))
 
+'''
+Ejercicio de intercambio de datos
+
+Se necesita diseñar un formato para intercambiar datos de estaciones meteorológicas. El sistema tiene N
+estaciones cada una con su localización (latitud y longitud) y un nombre alfanumérico. Las estaciones pueden
+tener diferentes combinaciones de sensores entre: humedad, dirección del viento, velocidad del viento,
+temperatura (que puede estar en grados celsius o fahrenheit) y presión atmosférica. No todas las estaciones
+tienen todos los sensores, pero al menos tienen uno. Los sensores deben almacenar por separado la
+magnitud leída de su unidad de medición.
+Además cada estación almacena el voltaje en milivolts de su batería una vez por segundo y el formato de
+archivo debe mantener las últimas 20 mediciones.
+
+1. A partir del nombre de la estación, computar la cantidad de sensores disponible y mostrar por pantalla
+los diferentes sensores, cada uno deberá mostrar el tipo y la variable medida:
+
+2.Calcular cuál es la estación con menos batería, es decir, la estación con menor valor promedio de
+voltaje.
+
+'''
 from functools import reduce
 
 resultado_final = reduce(lambda a, b: a+b, lista_resultados)
 print("El resultado final es:", resultado_final)
-
-
-
-
-
-
-
-
-'''
-Ejercicio de intercambio de datos
-'''
 
 import xml.etree.ElementTree as ET
 
